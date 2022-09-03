d338e3842d40d23eaf999c54a25f8c238b756906
Shubham Goyal <shubham.goyal2609@gmail.com>
2022-09-03 22:36:34 +0530

    Added exception handler for HttpException and pydantic 422

--- catalog_service/app/catalog_app.py
+++ catalog_service/app/catalog_app.py
@@ -1,36 +1,13 @@
 # from loguru import logger
 from fastapi import FastAPI
 
-from app.api import catalogs
+from starlette.exceptions import HTTPException
+from fastapi.exceptions import RequestValidationError
+
+from app.api import category
 from app.core.config import settings
-
-from app.db.mongodb import connect_to_database, close_mongo_connection
+from app.core.errors import http_error_handler, http_422_error_handler
 from app.core.events import create_start_app_handler, create_stop_app_handler
-
-# def create_application() -> FastAPI:
-#     application = FastAPI(
-#         openapi_url="/catalogs/openapi.json",
-#         docs_url="/catalogs/docs"
-#         )
-#     application.include_router(
-#         catalogs.router,
-#         tags=["catalogs"])
-#     return application
-
-
-# app = create_application()
-
-# @app.on_event("startup")
-# async def startup_event():
-#     logger.info("Starting up...")
-#     await connect_to_database()
-
-
-# @app.on_event("shutdown")
-# async def shutdown_event():
-#     logger.info("Shutting down...")
-#     await close_mongo_connection()
-
 
 def create_application() -> FastAPI:
     settings.configure_logging()
@@ -54,11 +31,11 @@ def create_application() -> FastAPI:
         create_stop_app_handler(),
     )
 
-    # application.add_exception_handler(HTTPException, http_error_handler)
-    # application.add_exception_handler(RequestValidationError, http422_error_handler)
+    application.add_exception_handler(HTTPException, http_error_handler)
+    application.add_exception_handler(RequestValidationError, http_422_error_handler)
     application.include_router(
-        catalogs.router,
-        tags=["catalogs"])
+        category.router,
+        tags=["categories"])
     # application.include_router(api_router, prefix=settings.api_prefix)
 
     return application
---
+++ catalog_service/app/core/errors.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+from fastapi.exceptions import RequestValidationError
+from fastapi.openapi.constants import REF_PREFIX
+from fastapi.openapi.utils import validation_error_response_definition
+from pydantic import ValidationError
+from starlette.exceptions import HTTPException
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_400_BAD_REQUEST
+
+
+async def http_error_handler(request: Request, exc: HTTPException) -> JSONResponse:
+    return JSONResponse({"errors": [exc.detail]})
+
+
+async def http_422_error_handler(
+    _: Request,
+    exc: Union[RequestValidationError, ValidationError],
+) -> JSONResponse:
+    return JSONResponse(
+        {"errors": exc.errors()},
+        status_code=HTTP_400_BAD_REQUEST,
+    )
